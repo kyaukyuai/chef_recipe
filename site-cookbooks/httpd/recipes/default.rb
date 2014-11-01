446b1a311b8e2b1c76d3099aace5fdb7e043253e
Yuya Kakui <mail@kakuiyuya.com>
2014-11-01 23:55:14 +0900

    stop iptables

--- site-cookbooks/httpd/recipes/default.rb
+++ site-cookbooks/httpd/recipes/default.rb
@@ -1,3 +1,16 @@
 package "httpd" do
   action :install
 end
+
+service "httpd" do
+  action [ :enable, :start ]
+end
+
+template "httpd.conf" do
+  path "/etc/httpd/httpd.conf"
+  owner "root"
+  group "root"
+  mode 0644
+
+  notifies :reload, "service[httpd]"
+end
